739c5841a22a2daaa71b8340b1936ed3e9963892
MaverickSamurai <mirsamirba@code.edu.az>
2022-06-21 16:12:23 +0400

    String-NameReverse

---
+++ HomeWorkString/HomeWorkString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HomeWorkString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NameReverse("Abqulqadir"));
+            
+        }
+    
+    
+    
+    
+    
+    
+    static string NameReverse(string name)
+            
+        {
+            string result = "";
+            for (int i = name.Length - 1; i >= 0; i--)
+                result += name[i];
+            return result;
+
+        
+        
+        }
+        
+    
+    }
+}
